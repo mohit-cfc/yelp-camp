eb3e00d28bc410e0f157563aa91a73e8a0350b83
mohit-cfc <santosh.mohit9@gmail.com>
2022-04-09 17:52:37 +0530

    port

--- index.js
+++ index.js
@@ -116,6 +116,6 @@ app.use((err, req, res, next) => {
   res.status(status).render("error", { err });
 });
 
-app.listen(3000, () => {
-  console.log("Listening to port 3000");
+app.listen(9000, () => {
+  console.log("Listening to port 9000");
 });
